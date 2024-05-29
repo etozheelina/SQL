2b5a5b6c89ec020b5562c73d2db17af73a0f53f8
Элина <buzinga25@gmail.com>
2024-05-30 03:44:48 +0500

    исправлен скрипт

--- table/lesson_5_task_2.sql
+++ table/lesson_5_task_2.sql
@@ -1,6 +1,13 @@
-CREATE TABLE Студенты (
-    Идентификатор INT PRIMARY KEY IDENTITY(1,1),
-    Имя NVARCHAR(100) NOT NULL,
-    Фамилия NVARCHAR(100) NOT NULL
+IF NOT EXISTS (
+    SELECT * 
+    FROM INFORMATION_SCHEMA.TABLES 
+    WHERE TABLE_NAME = 'Студенты'
+)
+BEGIN
+    CREATE TABLE Студенты (
+        Идентификатор INT PRIMARY KEY IDENTITY(1,1),
+        Имя NVARCHAR(100) NOT NULL,
+        Фамилия NVARCHAR(100) NOT NULL
 
-);
+    );
+END;
